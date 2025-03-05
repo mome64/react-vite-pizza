0cddb4e5f1597ee382efce665bf17c22dfeb7e59
Mohammed Mesoud <mesoudmohammed393@gmail.com>
2025-03-05 15:22:34 +0300

    first commit

---
+++ my-vue-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Pizza from "./Pizza";
+
+function App() {
+  return (
+    <div className="bg-stone-200 text-black  w-screen h-screen">
+      <Pizza />
+    </div>
+  );
+}
+
+export default App;
